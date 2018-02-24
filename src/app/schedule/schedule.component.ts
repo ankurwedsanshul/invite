47403890f2beb488d151faf840e705af4bcc5eca
Sumit Mundra <sumit.mundra.iitr@gmail.com>
2018-02-25 01:16:24 +0530

    schedule updated with full invite url choice

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common'
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}],
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
-
-  constructor() { }
+  location: Location;
+  fullInvite: boolean = false;
+  constructor(location: Location) {
+    this.location = location;
+  }
 
   ngOnInit() {
   }
 
+  isFullInvite(): boolean  {
+    return this.location.path(false) == '/invite';
+  }
 }
